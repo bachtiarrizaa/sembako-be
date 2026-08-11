f7c71877ed0821680d81a15b4e8bf2fde846846c
bachtiar <bachtiarriza87@gmail.com>
2026-08-11 19:11:26 +0700

    feat(user): implement profile update API and image upload integration

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -27,6 +27,11 @@ type UpdateStatusUserRequest struct {
 	IsActive bool `json:"isActive"`
 }
 
+type UpdateProfileRequest struct {
+	Name     *string `json:"name" form:"name" validate:"omitempty,min=2,max=150"`
+	Username *string `json:"username" form:"username" validate:"omitempty,min=2,max=100"`
+}
+
 type UserWithRole struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
@@ -37,6 +42,7 @@ type UserResponse struct {
 	Name      string       `json:"name"`
 	Email     string       `json:"email"`
 	Username  *string      `json:"username"`
+	Image     *string      `json:"image"`
 	Role      UserWithRole `json:"role"`
 	IsActive  bool         `json:"isActive"`
 	CreatedAt time.Time    `json:"createdAt"`
@@ -49,6 +55,7 @@ func ToUserResponse(u *entity.User) UserResponse {
 		Name:     u.Name,
 		Email:    u.Email,
 		Username: u.Username,
+		Image:    u.Image,
 		Role: UserWithRole{
 			ID:   u.Role.ID,
 			Name: u.Role.Name,
